7854dcb81fc89014155317e9042a2ce0f8d825bb
jason-greenberg <jasongreenberg07@gmail.com>
2023-01-24 09:06:00 -0800

    Combine gpt data in cover letter endpoint

--- backend/routes/api/coverletters.js
+++ backend/routes/api/coverletters.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { CoverLetter, Resume } = require('../../db/models');
+const { CoverLetter, Resume, Application } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 const { requestCoverLetterFromGPT3 } = require('../../utils/gpt3');
 
@@ -24,7 +24,7 @@ router.post(
   requireAuth,
   async (req, res, next) => {
     const userId = req.user.id;
-    const { resumeId } = req.body;
+    const { resumeId, jobDescription } = req.body;
 
     const resume = await Resume.findByPk(+resumeId);
     // 403 Forbidden Error if attached resume does not belong to user
@@ -37,8 +37,26 @@ router.post(
 
     // call gpt api
     try {
-      const data = await requestCoverLetterFromGPT3(resume, 'say hello')
-      res.json(data.choices[0].text);
+      const data = await requestCoverLetterFromGPT3(resume, jobDescription);
+      const letterText = data.choices[0].text;
+      const engine = data.model;
+
+      // Create new coverletter in db
+      const newCoverLetter = await CoverLetter.create({
+        userId,
+        letterText,
+        engine,
+        jobDescription
+      });
+
+      // Create new application to coincide with new coverletter and resume
+      const newApplication = await Application.create({
+        userId,
+        resumeId: +resumeId,
+        coverLetterId: newCoverLetter.id
+      });
+
+      res.json(newApplication);
     } catch (error) {
       res.json(error);
     }
