ad6a17dc0ddf405cdbd9e52c7179453d46a6f98a
jason-greenberg <jasongreenberg07@gmail.com>
2023-01-23 17:09:03 -0800

    Add get all current user's coverletters endpoint

--- backend/routes/api/coverletters.js
+++ backend/routes/api/coverletters.js
@@ -1,4 +1,31 @@
 const router = require('express').Router()
 const { CoverLetter } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+
+// Get all cover letters of current user
+router.get(
+  '/current',
+  requireAuth,
+  async (req, res, next) => {
+    const userId = req.user.id;
+    
+    // Query db for user's coverletters
+    const userCoverLetters = await CoverLetter.findAll({ where: { userId }});
+
+    // Return cover letters
+    res.json(userCoverLetters);
+  }
+);
+
+// Create new cover letter
+router.post(
+  '/',
+  requireAuth,
+  async (req, res, next) => {
+    const userId = req.user.id;
+
+    
+  }
+)
 
 module.exports = router;
